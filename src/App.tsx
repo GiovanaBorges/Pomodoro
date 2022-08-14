be3a6984812fdefd2744956ab67630c32819a94f
GiovanaBorges <gigi12345as17@gmail.com>
2022-08-14 14:12:32 -0300

    a fews changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from "react";
+import Layout from "./pages/layout";
 
 function App() {
-  return (
-    <>
-      <h1>aaaaaaaa</h1>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
