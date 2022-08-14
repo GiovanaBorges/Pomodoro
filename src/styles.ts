1a9babdfe9c650d370143d62594c4d1152b9b771
GiovanaBorges <gigi12345as17@gmail.com>
2022-08-14 14:11:04 -0300

    added props change bg color

---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+export const styleColors = {
+  pomodoro: {
+    bgcolor: "#DA4747",
+    color: "#F86A6A",
+  },
+  short: {
+    bgcolor: "#CB6DB6",
+    color: "#F178B6",
+  },
+  long: {
+    bgcolor: "#3B7FCF",
+    color: "#6197D8",
+  },
+};
