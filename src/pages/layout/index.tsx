ddb439e2f97f9c6ff618079995925b32dfbdf385
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-08-19 15:23:54 -0300

    add number counter and changed styles btn

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,20 +1,30 @@
 import { LayoutContent, Navbar, Timer, Button } from "./layout.styles";
 import { styleColors } from "../../styles";
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 
 function Layout() {
-  const [btnText, setBtnText] = useState("Start");
   const [bgColor, setbgColor] = useState("#DA4747");
   const [color, setColor] = useState("#F86A6A");
-  const [time,setTime] = useState("25:00")
+  const [time,setTime] = useState<number>(25)
 
-  function textStart() {
-    btnText === "Start" ? setBtnText("Stop") : setBtnText("Start");
-    show();
+  const horaAtual = useRef<number>()
+  const [timeStart,setTimeStart] = useState(false)
+  const [timeTest,setTimeTest] = useState(25)
+  
+
+  function StartTime() {
+    new Audio('/assets/cavalo-ratinho.mp3').play();
+    setTimeStart(true)
+    horaAtual.current = window.setInterval(()=>{
+      setTimeTest((prev) => prev -1)
+    },1000);
+
   }
 
-  function show() {
-    console.log(styleColors);
+  function StopTime(){
+    new Audio('/assets/pause.mp3').play();
+    window.clearInterval(horaAtual.current)
+    setTimeStart(false)
   }
 
   const changeColor = {
@@ -22,16 +32,19 @@ function Layout() {
       setbgColor(styleColors.pomodoro.bgcolor);
       setColor(styleColors.pomodoro.color);
       setTime(styleColors.pomodoro.time)
+      setTimeTest(styleColors.pomodoro.time)
     },
     short() {
       setbgColor(styleColors.short.bgcolor);
       setColor(styleColors.short.color);
       setTime(styleColors.short.time)
+      setTimeTest(styleColors.short.time)
     },
     long() {
       setbgColor(styleColors.long.bgcolor);
       setColor(styleColors.long.color);
       setTime(styleColors.long.time)
+      setTimeTest(styleColors.long.time)
     },
   };
 
@@ -47,8 +60,14 @@ function Layout() {
             <li onClick={changeColor.short}>Short break</li>
             <li onClick={changeColor.long}>Long break</li>
           </ul>
-          <h1>{time}</h1>
-          <Button onClick={textStart} bg={bgColor}>{btnText}</Button>
+          <h1>{timeTest}</h1>
+          {timeStart? (
+            <Button onClick={StopTime} shadow={timeStart} bg={bgColor} >Stop</Button>
+          ):(
+            <Button onClick={StartTime} shadow={timeStart} bg={bgColor} >Start</Button>
+          )}
+          
+          
         </Timer>
       </LayoutContent>
     </>
