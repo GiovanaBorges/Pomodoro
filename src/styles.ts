76d729764e8ad332ce07403b5ff3e975991466e0
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-08-19 15:22:53 -0300

    testing number

--- src/styles.ts
+++ src/styles.ts
@@ -2,16 +2,16 @@ export const styleColors = {
   pomodoro: {
     bgcolor: "#DA4747",
     color: "#F86A6A",
-    time:"25:00"
+    time:25
   },
   short: {
     bgcolor: "#CB6DB6",
     color: "#F178B6",
-    time:"05:00"
+    time:5
   },
   long: {
     bgcolor: "#3B7FCF",
     color: "#6197D8",
-    time:"15:00"
+    time:15
   },
 };
