a071d794b209bee72b199416d4a7ff310dcec432
GiovanaBorges <gigi12345as17@gmail.com>
2022-08-21 13:26:16 -0300

    added verify time end and start

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,50 +1,72 @@
 import { LayoutContent, Navbar, Timer, Button } from "./layout.styles";
 import { styleColors } from "../../styles";
 import { useEffect, useState,useRef } from "react";
+import {Count} from "../../count"
 
 function Layout() {
   const [bgColor, setbgColor] = useState("#DA4747");
   const [color, setColor] = useState("#F86A6A");
-  const [time,setTime] = useState<number>(25)
+  //const [time,setTime] = useState<String>('25:00')
+  const [minutes,setMinutes] = useState<number>(25)
+  const [seconds,setSeconds] = useState<number>(0)
 
-  const horaAtual = useRef<number>()
+  const timeRef = useRef<number>()
   const [timeStart,setTimeStart] = useState(false)
-  const [timeTest,setTimeTest] = useState(25)
   
 
   function StartTime() {
-    new Audio('/assets/cavalo-ratinho.mp3').play();
+    new Audio('/assets/click-stop.wav').play();
     setTimeStart(true)
-    horaAtual.current = window.setInterval(()=>{
-      setTimeTest((prev) => prev -1)
-    },1000);
+    Count()
+  }
 
+  function Count(){
+    let seconds = 15
+    let minutesCurrent = minutes
+
+    timeRef.current = window.setInterval(()=>{
+      seconds -=1
+      setSeconds(seconds)
+
+      if(seconds === 0){
+        if(minutesCurrent === 0 && seconds === 0 ){
+          window.clearInterval(timeRef.current)
+          alert("the time is over")
+          new Audio('/assets/congratualations.wav').play()
+          window.location.reload()
+          StopTime()
+        }else{
+          
+        setSeconds(seconds = 15)
+        setMinutes(minutesCurrent -=1)
+        }
+      }
+
+    },1000);
   }
 
   function StopTime(){
-    new Audio('/assets/pause.mp3').play();
-    window.clearInterval(horaAtual.current)
+    new Audio('/assets/click-start.wav').play();
+    window.clearInterval(timeRef.current)
     setTimeStart(false)
+    
   }
 
   const changeColor = {
     pomodoro() {
       setbgColor(styleColors.pomodoro.bgcolor);
       setColor(styleColors.pomodoro.color);
-      setTime(styleColors.pomodoro.time)
-      setTimeTest(styleColors.pomodoro.time)
+      setMinutes(parseInt(styleColors.pomodoro.time.split(":",1).join("")))
     },
     short() {
       setbgColor(styleColors.short.bgcolor);
       setColor(styleColors.short.color);
-      setTime(styleColors.short.time)
-      setTimeTest(styleColors.short.time)
+      setMinutes(parseInt(styleColors.short.time.split(":",1).join("")))
     },
     long() {
       setbgColor(styleColors.long.bgcolor);
       setColor(styleColors.long.color);
-      setTime(styleColors.long.time)
-      setTimeTest(styleColors.long.time)
+      setMinutes(parseInt(styleColors.long.time.split(":",1).join("")))
     },
   };
 
@@ -60,7 +82,7 @@ function Layout() {
             <li onClick={changeColor.short}>Short break</li>
             <li onClick={changeColor.long}>Long break</li>
           </ul>
-          <h1>{timeTest}</h1>
+          <h1>{minutes} : {seconds}</h1>
           {timeStart? (
             <Button onClick={StopTime} shadow={timeStart} bg={bgColor} >Stop</Button>
           ):(
