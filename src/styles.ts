84e19edf2f772b559a5d8c13a08ef09bc00f5657
GiovanaBorges <gigi12345as17@gmail.com>
2022-08-21 13:25:54 -0300

    changed type of time

--- src/styles.ts
+++ src/styles.ts
@@ -2,16 +2,16 @@ export const styleColors = {
   pomodoro: {
     bgcolor: "#DA4747",
     color: "#F86A6A",
-    time:25
+    time:'25:00'
   },
   short: {
     bgcolor: "#CB6DB6",
     color: "#F178B6",
-    time:5
+    time:'05:00'
   },
   long: {
     bgcolor: "#3B7FCF",
     color: "#6197D8",
-    time:15
+    time:'15:00'
   },
 };
